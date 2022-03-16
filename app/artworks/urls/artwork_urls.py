7f9f5a9207a82ab7061240c0157a4119161c6060
Ehsan G <ehsan@say.company>
2022-03-15 21:47:19 -0400

    carousel admin page

--- app/artworks/urls/artwork_urls.py
+++ app/artworks/urls/artwork_urls.py
@@ -3,14 +3,15 @@ from artworks.views import artwork_views as views
 
 
 urlpatterns = [
-    path('getSubcategory/', views.get_subcategory, name='sub_category_list'),
-    path('categories/', views.categories, name='category_list'),
-    path('', views.fetchArtworkList, name='artworks'),
-    path('origins/', views.fetch_origin_list, name='origins'),
-    path('delete/', views.delete_the_artwork, name='artwork_delete'),
-    path('voucher/<int:pk>/delete/',
-         views.delete_the_voucher, name='voucher_delete'),
-    path('update/<int:pk>/<str:action>/',
-         views.update_the_artwork, name='artwork_update'),
-    path('<int:pk>/', views.fetch_the_artwork, name='the_artWork'),
+    path("getSubcategory/", views.get_subcategory, name="sub_category_list"),
+    path("categories/", views.categories, name="category_list"),
+    path("", views.fetchArtworkList, name="artworks"),
+    path("carousels", views.fetch_is_carousel, name="carousels"),
+    path("origins/", views.fetch_origin_list, name="origins"),
+    path("delete/", views.delete_the_artwork, name="artwork_delete"),
+    path("voucher/<int:pk>/delete/", views.delete_the_voucher, name="voucher_delete"),
+    path(
+        "update/<int:pk>/<str:action>/", views.update_the_artwork, name="artwork_update"
+    ),
+    path("<int:pk>/", views.fetch_the_artwork, name="the_artWork"),
 ]
--- app/artworks/views/artwork_views.py
+++ app/artworks/views/artwork_views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from artworks.serializer import ArtworkSerializer, OrderSerializer, OriginSerializer, TheTokenSerializer, VoucherSerializer
+from artworks.serializer import (
+    ArtworkSerializer,
+    OrderSerializer,
+    OriginSerializer,
+    TheTokenSerializer,
+    VoucherSerializer,
+)
 from django.contrib.auth.models import User
-from artworks.models import Artwork, Artist, Category, MyUser, Order, Origin, ShippingAddress, SubCategory, TheMarketPlace, TheToken, Voucher
+from artworks.models import (
+    Artwork,
+    Artist,
+    Category,
+    MyUser,
+    Order,
+    Origin,
+    ShippingAddress,
+    SubCategory,
+    TheMarketPlace,
+    TheToken,
+    Voucher,
+)
 from rest_framework import status
 from artworks.serializer import CategorySerializer
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -16,70 +34,63 @@ import requests
 # for admin and change_form.html
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_subcategory(request):
-    id = request.GET.get('id', '')
-    result = list(SubCategory.objects.filter(
-        category_id=int(id)).values('_id', 'name'))
+    id = request.GET.get("id", "")
+    result = list(SubCategory.objects.filter(category_id=int(id)).values("_id", "name"))
     return HttpResponse(json.dumps(result), content_type="application/json")
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def categories(request):
     categories = Category.objects.all()
     serializer = CategorySerializer(categories, many=True)
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def fetch_origin_list(request):
     origins = Origin.objects.all()
     serializer = OriginSerializer(origins, many=True)
-    return Response({'origins': serializer.data})
+    return Response({"origins": serializer.data})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def fetchArtworkList(request):
-    query = request.query_params.get('keyword')
-    page = request.query_params.get('page')
-    query_region = request.query_params.get('regions')
-    query_artist = request.query_params.get('artist')
-    query_category = request.query_params.get('category')
-    query_on_sale = request.query_params.get('onMarket')
+    query = request.query_params.get("keyword")
+    page = request.query_params.get("page")
+    query_region = request.query_params.get("regions")
+    query_artist = request.query_params.get("artist")
+    query_category = request.query_params.get("category")
+    query_on_sale = request.query_params.get("onMarket")
 
     if query_on_sale:
-        artworks = Artwork.objects.filter(
-            on_market=True).order_by('created_at')
+        artworks = Artwork.objects.filter(on_market=True).order_by("created_at")
         serializer = ArtworkSerializer(artworks, many=True)
-        return Response({'artworks': serializer.data})
+        return Response({"artworks": serializer.data})
 
     if query_category:
         category = Category.objects.get(_id=query_category)
-        artworks = Artwork.objects.filter(
-            category=category).order_by('created_at')
+        artworks = Artwork.objects.filter(category=category).order_by("created_at")
         serializer = ArtworkSerializer(artworks, many=True)
-        return Response({'artworks': serializer.data})
+        return Response({"artworks": serializer.data})
 
     if query_artist:
         artist = Artist.objects.get(_id=query_artist)
-        artworks = Artwork.objects.filter(
-            artist=artist).order_by('created_at')
+        artworks = Artwork.objects.filter(artist=artist).order_by("created_at")
         serializer = ArtworkSerializer(artworks, many=True)
-        return Response({'artworks': serializer.data})
+        return Response({"artworks": serializer.data})
 
     elif query_region:
-        origin = Origin.objects.filter(
-            country__icontains=query_region).first()
-        artworks = Artwork.objects.filter(
-            origin=origin).order_by('created_at')
+        origin = Origin.objects.filter(country__icontains=query_region).first()
+        artworks = Artwork.objects.filter(origin=origin).order_by("created_at")
         serializer = ArtworkSerializer(artworks, many=True)
-        return Response({'artworks': serializer.data})
+        return Response({"artworks": serializer.data})
 
     elif query == None:
-        query = ''
+        query = ""
         # we could use any value instead of title
-        artworks = Artwork.objects.filter(
-            title__icontains=query).order_by('created_at')
+        artworks = Artwork.objects.filter(title__icontains=query).order_by("created_at")
         # pagination
         p = Paginator(artworks, 10)
 
@@ -96,17 +107,26 @@ def fetchArtworkList(request):
         page = int(page)
 
         serializer = ArtworkSerializer(artworks, many=True)
-        return Response({'artworks': serializer.data, 'page': page, 'pages': p.num_pages})
+        return Response(
+            {"artworks": serializer.data, "page": page, "pages": p.num_pages}
+        )
 
 
-@ api_view(['GET'])
+@api_view(["GET"])
 def fetch_the_artwork(request, pk):
     artwork = Artwork.objects.get(_id=pk)
     serializer = ArtworkSerializer(artwork, many=False)
     return Response(serializer.data)
 
 
-@api_view(['PUT'])
+@api_view(["GET"])
+def fetch_is_carousel(request):
+    artworks = Artwork.objects.filter(is_carousel=True).order_by("created_at")
+    serializer = ArtworkSerializer(artworks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def update_the_artwork(request, pk, action):
     user = request.user
@@ -114,17 +134,17 @@ def update_the_artwork(request, pk, action):
     artwork = Artwork.objects.get(_id=pk)
 
     # 1 - Action Sign: update product when sign the product
-    if user and action == 'Signing':
+    if user and action == "Signing":
         voucher = Voucher.objects.create(
-            title=data['title'],
-            artwork_id=int(data['artworkId']),
-            edition_number=data['editionNumber'],
-            edition=data['edition'],
-            price_wei=data['priceWei'],
-            price_dollar=data['priceDollar'],
-            token_Uri=data['tokenUri'],
-            content=data['content'],
-            signature=data['signature']
+            title=data["title"],
+            artwork_id=int(data["artworkId"]),
+            edition_number=data["editionNumber"],
+            edition=data["edition"],
+            price_wei=data["priceWei"],
+            price_dollar=data["priceDollar"],
+            token_Uri=data["tokenUri"],
+            content=data["content"],
+            signature=data["signature"],
         )
         voucher.save()
 
@@ -134,15 +154,15 @@ def update_the_artwork(request, pk, action):
         artwork.is_minted = False
         artwork.save()
         serializer = VoucherSerializer(voucher, many=False)
-        return Response({'voucher': serializer.data})
+        return Response({"voucher": serializer.data})
 
     # 2 - Action Redeem and Mint: update product when mint the product
-    elif user and action == 'RedeemAndMint':
+    elif user and action == "RedeemAndMint":
         # a - create NFT
         token = TheToken.objects.create(
             market_item_id=None,
-            token_id=data['tokenId'],
-            contract=data['galleryAddress'],
+            token_id=data["tokenId"],
+            contract=data["galleryAddress"],
         )
         token.artwork = artwork
         token.holder = user
@@ -150,9 +170,9 @@ def update_the_artwork(request, pk, action):
 
         # b - create order
         order = Order.objects.create(
-            transaction_hash=data['transactionHash'],
-            price_eth=data['priceEth'],
-            fee_eth=data['feeEth'],
+            transaction_hash=data["transactionHash"],
+            price_eth=data["priceEth"],
+            fee_eth=data["feeEth"],
         )
         order.seller = artwork.owner
         order.buyer = user
@@ -161,12 +181,12 @@ def update_the_artwork(request, pk, action):
 
         # c - relate the shipping address and order
         shipping_address = ShippingAddress.objects.create(
-            address=data['address'],
-            postal_code=data['postalCode'],
-            city=data['city'],
-            province=data['province'],
-            country=data['country'],
-            phone=data['phoneNumber']
+            address=data["address"],
+            postal_code=data["postalCode"],
+            city=data["city"],
+            province=data["province"],
+            country=data["country"],
+            phone=data["phoneNumber"],
         )
         shipping_address.order = order
         shipping_address.buyer = user
@@ -188,21 +208,21 @@ def update_the_artwork(request, pk, action):
         voucher.delete()
 
         serializer = OrderSerializer(order, many=False)
-        return Response({'order': serializer.data})
+        return Response({"order": serializer.data})
 
 
-@ api_view(['DELETE'])
-@ permission_classes([IsAdminUser])
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])
 def delete_the_artwork(request):
     data = request.data
-    selectedArtworks = data['selectedArtworks']
+    selectedArtworks = data["selectedArtworks"]
     for _id in selectedArtworks:
         artworkDeleting = Artwork.objects.get(_id=_id)
         artworkDeleting.delete()
-    return Response('artworks were deleted')
+    return Response("artworks were deleted")
 
 
-@ api_view(['DELETE'])
+@api_view(["DELETE"])
 @permission_classes([IsAuthenticated])
 def delete_the_voucher(request, pk):
     voucher = Voucher.objects.get(_id=pk)
@@ -216,4 +236,4 @@ def delete_the_voucher(request, pk):
 
     voucher.delete()
 
-    return Response('signature was deleted')
+    return Response("signature was deleted")
