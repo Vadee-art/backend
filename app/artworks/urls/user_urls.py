a6bebd2ab08c9772ad316236151bcb9698ecb770
Ehsan G <ehsan@say.company>
2022-06-22 01:50:54 -0400

    artist fav

--- app/artworks/urls/user_urls.py
+++ app/artworks/urls/user_urls.py
@@ -3,18 +3,25 @@ from artworks.views import user_views as views
 
 
 urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('register/', views.registerUser, name='register'),
-    path('profile/', views.fetchUserProfile, name='users_profile'),
-    path('profile/update/', views.updateUserProfile, name='users_profile_update'),
-    path('profile/artworks/mine', views.fetchMyArtworks, name='users_profile_artworks'),
-    path('profile/artworks/favorites/', views.fetchFavoriteArtworkList, name='favorite_artworks'),
-    path('profile/artists/favorites/', views.fetchFavoriteArtistList, name='favorite_artists'),
-    path('artwork/favorite/<int:pk>/', views.addFavoriteArtwork, name='favorite_add'),
-    path('artist/favorite/<int:pk>/', views.addFavoriteArtwork, name='favorite_add'),
+    path("login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("register/", views.registerUser, name="register"),
+    path("profile/", views.fetchUserProfile, name="users_profile"),
+    path("profile/update/", views.updateUserProfile, name="users_profile_update"),
+    path("profile/artworks/mine", views.fetchMyArtworks, name="users_profile_artworks"),
+    path(
+        "profile/artworks/favorites/",
+        views.fetchFavoriteArtworkList,
+        name="favorite_artworks",
+    ),
+    path(
+        "profile/artists/favorites/",
+        views.fetchFavoriteArtistList,
+        name="favorite_artists",
+    ),
+    path("artwork/favorite/<int:pk>/", views.addFavoriteArtwork, name="favorite_add_artwork"),
+    path("artist/favorite/<int:pk>/", views.addFavoriteArtist, name="favorite_add_artist"),
     # path('update/<int:pk>/', views.updateUserById, name='user_update_by_id'),
-    path('', views.fetchUsers, name='users'),
-    path('delete/', views.deleteUser, name='user_delete'),
-    path('<int:pk>/', views.fetchUsersById, name='user_by_id'),
+    path("", views.fetchUsers, name="users"),
+    path("delete/", views.deleteUser, name="user_delete"),
+    path("<int:pk>/", views.fetchUsersById, name="user_by_id"),
 ]
--- app/artworks/views/user_views.py
+++ app/artworks/views/user_views.py
@@ -4,7 +4,12 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from artworks.serializer import ArtistSerializer, ArtworkSerializer, UserSerializer, UserSerializerWithToken
+from artworks.serializer import (
+    ArtistSerializer,
+    ArtworkSerializer,
+    UserSerializer,
+    UserSerializerWithToken,
+)
 from artworks.models import Artwork, Artist, MyUser
 from rest_framework import status
 
@@ -20,8 +25,8 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         # Add custom claims, meaning when token decrypted more data is available check jwt.io
-        token['username'] = user.user_name
-        token['message'] = 'hello world'
+        token["username"] = user.user_name
+        token["message"] = "hello world"
         return token
 
     def validate(self, attrs):
@@ -41,56 +46,56 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def registerUser(request):
     data = request.data
     try:
         user = MyUser.objects.create(
-            first_name=data['firstName'],
-            last_name=data['lastName'],
-            user_name=data['email'],
-            email=data['email'],
-            password=make_password(data['password'])
-
+            first_name=data["firstName"],
+            last_name=data["lastName"],
+            user_name=data["email"],
+            email=data["email"],
+            password=make_password(data["password"]),
         )
         serializer = UserSerializerWithToken(user, many=False)
         return Response(serializer.data)
     except:
-        message = {'detail: User with this email already exists'}
+        message = {"detail: User with this email already exists"}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PUT'])
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def updateUserProfile(request):
     user = request.user
 
     data = request.data
-    user.first_name = data['firstName']
-    user.last_name = data['lastName']
-    if 'checked' in data:
-        user.country = data['country']
-        user.city = data['city']
-        user.province = data['province']
-        user.phone_number = data['phoneNumber']
-        user.postal_code = data['postalCode']
-        user.address = data['address']
-    if 'email' in data:
-        user.email = data['email']
-        user.username = data['email']
-    if (('password' in data) and (data['password'] != '')):
-        user.password = make_password(data['password'])
+    user.first_name = data["firstName"]
+    user.last_name = data["lastName"]
+    if "checked" in data:
+        user.country = data["country"]
+        user.city = data["city"]
+        user.province = data["province"]
+        user.phone_number = data["phoneNumber"]
+        user.postal_code = data["postalCode"]
+        user.address = data["address"]
+    if "email" in data:
+        user.email = data["email"]
+        user.username = data["email"]
+    if ("password" in data) and (data["password"] != ""):
+        user.password = make_password(data["password"])
 
     user.save()
     serializer = UserSerializerWithToken(user, many=False)
 
     return Response(serializer.data)
 
+
 # since we have wrapped this view with rest-framework decorator and changed the default authentication
 # /api/users/profile user is not the same as the user used for /admin
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def fetchUserProfile(request):
     user = request.user
@@ -98,7 +103,7 @@ def fetchUserProfile(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAdminUser])
 def fetchUsers(request):
     users = MyUser.objects.all()
@@ -106,7 +111,7 @@ def fetchUsers(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAdminUser])
 def fetchUsersById(request, pk):
     user = MyUser.objects.get(id=pk)
@@ -114,37 +119,37 @@ def fetchUsersById(request, pk):
     return Response(serializer.data)
 
 
-@api_view(['PUT'])
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def updateUserById(request, pk):
     user = MyUser.objects.get(id=pk)
 
     data = request.data
-    user.first_name = data['firstName']
-    user.last_name = data['lastName']
-    if 'email' in data:
-        user.email = data['email']
-        user.username = data['email']
-    user.is_staff = data['isAdmin']
+    user.first_name = data["firstName"]
+    user.last_name = data["lastName"]
+    if "email" in data:
+        user.email = data["email"]
+        user.username = data["email"]
+    user.is_staff = data["isAdmin"]
     user.save()
     serializer = UserSerializer(user, many=False)
 
     return Response(serializer.data)
 
 
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 @permission_classes([IsAdminUser])
 def deleteUser(request):
     data = request.data
-    selectedUsers = data['selectedUsers']
+    selectedUsers = data["selectedUsers"]
     for id in selectedUsers:
         userDeleting = MyUser.objects.get(id=id)
         userDeleting.delete()
-    return Response('users were deleted')
+    return Response("users were deleted")
 
 
 # remove or add artwork to your favorites
-@api_view(['PUT'])
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def addFavoriteArtwork(request, pk):
     try:
@@ -153,24 +158,24 @@ def addFavoriteArtwork(request, pk):
             artwork.favorites.remove(request.user)
         else:
             artwork.favorites.add(request.user)
-            message = {'detail: We could not make any changes!'}
+            message = {"detail: We could not make any changes!"}
 
         return Response(artwork._id)
     except:
-        message = {'detail: We could not make any changes!'}
+        message = {"detail: We could not make any changes!"}
         return Response(message, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def fetchFavoriteArtworkList(request):
     artworks = Artwork.objects.filter(favorites=request.user)
     serializer = ArtworkSerializer(artworks, many=True)
-    return Response({'favorites': serializer.data})
+    return Response({"favorites": serializer.data})
 
 
 # remove or add artists to your favorites
-@api_view(['PUT'])
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def addFavoriteArtist(request, pk):
     try:
@@ -179,33 +184,31 @@ def addFavoriteArtist(request, pk):
             artist.favorites.remove(request.user)
         else:
             artist.favorites.add(request.user)
-            message = {'detail: We could not make any changes!'}
+            message = {"detail: We could not make any changes!"}
 
         return Response(artist._id)
     except:
-        message = {'detail: We could not make any changes!'}
+        message = {"detail: We could not make any changes!"}
         return Response(message, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def fetchFavoriteArtistList(request):
     artists = Artist.objects.filter(favorites=request.user)
     serializer = ArtistSerializer(artists, many=True)
-    return Response({'favorites': serializer.data})
+    return Response({"favorites": serializer.data})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def fetchMyArtworks(request):
     try:
         user = request.user
         artworks = Artwork.objects.filter(owner=user)
         serializer = ArtworkSerializer(artworks, many=True)
-        return Response({'my_artworks': serializer.data})
+        return Response({"my_artworks": serializer.data})
 
     except:
-        message = {'details': 'No Artworks'}
+        message = {"details": "No Artworks"}
         return Response(message, status=status.HTTP_404_NOT_FOUND)
-
-
