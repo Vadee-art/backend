4210f83c2e049fb4d8c691d36c246f38114e2481
Arash Fatahzade <fatahzade@gmail.com>
2023-09-08 16:11:27 +0330

    Fix filters

--- app/artworks/filters.py
+++ app/artworks/filters.py
@@ -1,12 +1,19 @@
-from django_filters import CharFilter, NumberFilter
+from django_filters import BaseInFilter, CharFilter, NumberFilter
 from django_filters import rest_framework as filters
 
 from artworks.models import Artist, Artwork, Origin
 
 
+class NumberInFilter(BaseInFilter, NumberFilter):
+    pass
+
+
 class ArtworkFilter(filters.FilterSet):
     price__gt = NumberFilter(field_name="price", lookup_expr="gt")
     price__lt = NumberFilter(field_name="price", lookup_expr="lt")
+    origin = NumberInFilter(field_name='origin_id', lookup_expr='in')
+    category = NumberInFilter(field_name='category_id', lookup_expr='in')
+    sub_category = NumberInFilter(field_name='sub_category_id', lookup_expr='in')
 
     class Meta:
         model = Artwork
@@ -15,6 +22,7 @@ class ArtworkFilter(filters.FilterSet):
 
 class ArtistFilter(filters.FilterSet):
     name_starts = CharFilter(field_name="user__first_name", lookup_expr="istartswith")
+    origin = NumberInFilter(field_name='origin_id', lookup_expr='in')
 
     class Meta:
         model = Artist
