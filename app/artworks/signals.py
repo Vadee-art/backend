2aa823e3a3590860a5abc1282ba54dd54ddb87e5
Arash Fatahzade <fatahzade@gmail.com>
2024-04-21 00:40:31 +0330

    Fix add artwork

--- app/artworks/signals.py
+++ app/artworks/signals.py
@@ -24,11 +24,11 @@ def user_post_save(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Artwork)
 def artwork_post_save(sender, instance, created, **kwargs):
-    if created or instance.signature:
-        instance.sign()
-
     if not instance.image_ipfs_hash:
         instance.upload_image_to_ipfs()
 
     if not instance.metadata_ipfs_hash:
         instance.upload_metadata_to_ipfs()
+
+    if created or instance.signature:
+        instance.sign()
